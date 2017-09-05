102b1421834993ffd4f1da0a0a8e74c81ef930b5
KyungSeok <leeseok8347@gmail.com>
2017-09-05 21:53:26 +0900

    난이도1 문제 저장

--- Difficulty1/Test01.java
+++ Difficulty1/Test01.java
@@ -1,20 +1,19 @@
 
+/**
+ * 
+ * @author Kyung
+ * Controlflow 난이도1 예제 풀이
+ *
+ */
 public class Test01 {
 	
-	//입력 1 : 마크가 되는 문자열
-	//입력 2 : 출력할 줄 수
-	
-	//<결과>
-	//A
-	//AA
-	//AAA
-	//AAAA
-	//AAAAA
-	
-	//run("A",5)
-	
+	// 표시할 값, 표시할 라인 수 입력
 	public void run(String mark, int lines) {
+		
+		// 라인만큼 반복
 		for(int index=0 ; index<lines ; index=index+1) {
+			
+			// 입력될 값 표시
 			for(int innerIndex=0 ; innerIndex<index+1 ; innerIndex++) {
 				System.out.print(mark);
 			}
