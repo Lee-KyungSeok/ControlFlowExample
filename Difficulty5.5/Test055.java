ea1dcbfad5f1583f1695902078901059771aff98
KyungSeok <leeseok8347@gmail.com>
2017-09-06 11:58:11 +0900

    난이도5.5 저장

--- Difficulty5.5/Test055.java
+++ Difficulty5.5/Test055.java
@@ -1,10 +1,16 @@
 
+/**
+ * 
+ * @author Kyung
+ * Controlflow 난이도5.5 예제 풀이
+ *
+ */
 public class Test055 {
 	public void run(String mark, int lines) {
 		
 		for(int lineIndex =0 ; lineIndex<lines*2-1 ; lineIndex=lineIndex+1) {
 			
-			//위의 빈 트리 작성
+			//라인수가 입력값보다 작은 경우 실행(윗부분)
 			if(lineIndex<lines) {
 				
 				//처음 공백 출력
@@ -15,14 +21,11 @@ public class Test055 {
 				// 처음 mark 찍기
 				System.out.print(mark); 
 				
-				//가운데 공백 출력1
-				for(int innerSpaceIndex1=0 ; innerSpaceIndex1<lines-lineIndex-1 ; innerSpaceIndex1=innerSpaceIndex1+1) {
+				//가운데 공백 출력
+				for(int innerSpaceIndex1=0 ; innerSpaceIndex1<2*(lines-lineIndex)-3 ; innerSpaceIndex1=innerSpaceIndex1+1) {
 					System.out.print(" ");
 				}
-				//가운데 공백 출력2
-				for(int innerSpaceIndex2=0 ; innerSpaceIndex2<lines-lineIndex-2 ; innerSpaceIndex2=innerSpaceIndex2+1) {
-					System.out.print(" ");
-				}
+
 				//마지막 mark 찍기
 				if(lineIndex==lines-1) {
 					System.out.println("");
@@ -31,7 +34,7 @@ public class Test055 {
 				}
 			}
 			
-			//아래 빈 트리 작성
+			//라인수가 입력값보다 크거나 같은 경우 실행(아래부분)
 			else {
 				
 				//처음 공백 출력
@@ -43,12 +46,7 @@ public class Test055 {
 				System.out.print(mark);
 				
 				//가운데 공백 출력1
-				for(int downInnerSpaceIndex=0 ; downInnerSpaceIndex<lineIndex-lines+1 ; downInnerSpaceIndex=downInnerSpaceIndex+1) {
-					System.out.print(" ");
-				}
-				
-				//가운데 공백 출력2
-				for(int downInnerSpaceIndex=0 ; downInnerSpaceIndex<lineIndex-lines ; downInnerSpaceIndex=downInnerSpaceIndex+1) {
+				for(int downInnerSpaceIndex=0 ; downInnerSpaceIndex<2*(lineIndex-lines)+1 ; downInnerSpaceIndex=downInnerSpaceIndex+1) {
 					System.out.print(" ");
 				}
 				
